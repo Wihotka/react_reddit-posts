1802d0e93eee556b55e686dfa35d1fed70b9433c
Wihotka <drfinx@gmail.com>
2022-03-04 11:39:00 +0700

    initial commit

---
+++ HW2/src/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './header.scss';
+
+export function Header() {
+    return (
+        <header>
+            <h1 className={styles.example}>Hello React</h1>
+        </header>
+    );
+}

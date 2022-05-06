9735e57955843421d177163e741063067fc5b8ce
Wihotka <drfinx@gmail.com>
2022-05-06 20:32:15 +0700

    task-11 is completed

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,7 @@ import { Layout } from './Layout';
 import { Header } from './Header';
 import { Content } from './Content';
 import { CardsList } from './CardsList';
-import { useToken } from './hooks/useToken';
-import { tokenContext } from './context/tokenContext';
-import { UserContextProvider } from './context/userContext';
 import { PostsContextProvider } from './context/postsContext';
-
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
@@ -17,23 +13,16 @@ import { rootReducer } from './store';
 const store = createStore(rootReducer, composeWithDevTools());
 
 function AppComponent() {
-  const [token] = useToken();
-  const TokenProvider = tokenContext.Provider;
-
   return (
     <Provider store={store}>
-      <TokenProvider value={token}>
-        <UserContextProvider>
-          <Layout>
-            <Header />
-            <PostsContextProvider>
-              <Content>
-                <CardsList />
-              </Content>
-            </PostsContextProvider>
-          </Layout>
-        </UserContextProvider>
-      </TokenProvider>
+      <Layout>
+        <Header />
+        <PostsContextProvider>
+          <Content>
+            <CardsList />
+          </Content>
+        </PostsContextProvider>
+      </Layout>
     </Provider>
   );
 }
--- src/hooks/useUserData.tsx
+++ src/hooks/useUserData.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import { tokenContext } from '../context/tokenContext';
+import { useSelector } from 'react-redux';
+import { TRootState } from '../store';
 
 interface IUserData {
   name?: string;
@@ -9,7 +11,10 @@ interface IUserData {
 
 export function useUserData() {
   const [data, setData] = useState<IUserData>({});
-  const token = useContext(tokenContext);
+  // const token = useContext(tokenContext);
+
+  const token = useSelector<TRootState, string>(state => state.token);
+  console.log(token);
 
   useEffect(() => {
     axios.get('https://oauth.reddit.com/api/v1/me', {
